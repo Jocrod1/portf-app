0eeb3a160537827c3a2c58ec6cdaef90aa9feec5
Jose Pereira <jp@intelli-next.com>
2022-08-12 14:46:32 +0200

    Styles on socials icons

--- src/components/PersonalCard.tsx
+++ src/components/PersonalCard.tsx
@@ -12,13 +12,19 @@ const PersonalCard = () => {
       </div>
       <div className="flex flex-col justify-center px-11 items-center lg:items-start">
         <h1 className="font-extrabold text-6xl">José Pereira</h1>
-        <span className="text-center lg:text-left ">
+        <span className="text-left mb-5">
           Software developer based in Valencia, Spain
         </span>
         <div className="flex flex-row w-28 justify-between">
-          <Twitter />
-          <GitHub />
-          <Instagram />
+          <a href="http://www.twitter.com/jocrod1" target={"_blank"}>
+            <Twitter className="socials-icons" />
+          </a>
+          <a href="http://www.github.com/jocrod1" target={"_blank"}>
+            <GitHub className="socials-icons" />
+          </a>
+          <a href="http://www.instagram.com/jocrod1" target={"_blank"}>
+            <Instagram className="socials-icons ins" />
+          </a>
         </div>
       </div>
     </div>
