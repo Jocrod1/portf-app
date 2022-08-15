894979bd0598e5bb3c021725e24eb40bdd23cf30
Jose Pereira <jp@intelli-next.com>
2022-08-15 14:00:39 +0200

    DropDown component functional

--- src/components/SelectLang.tsx
+++ src/components/SelectLang.tsx
@@ -1,33 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChevronDown } from "react-feather";
 
 const SelectLang = () => {
+  const [open, setOpen] = useState<Boolean>(false);
   return (
     <div className="selectLangCont">
-      <button className="selectLangBtn">
+      <button
+        onClick={() => setOpen(!open)}
+        onBlur={() => setOpen(false)}
+        className="selectLangBtn"
+      >
         <span>English</span>
         <ChevronDown />
       </button>
-      <div className="z-10 w-28 bg-white rounded divide-y divide-gray-100 dark:bg-gray-700">
-        <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              English
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Español
-            </a>
-          </li>
-        </ul>
-      </div>
+      {open && (
+        <div className="selectList">
+          <ul className="selectListCont">
+            <li>
+              <a href="#" className="selectItem">
+                English
+              </a>
+            </li>
+            <li>
+              <a href="#" className="selectItem">
+                Español
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
